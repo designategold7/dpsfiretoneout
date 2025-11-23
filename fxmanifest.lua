2b9b53be8bf76952b5fb3cf42a13cea8fffe17f4
delineate <17305941+designategold7@users.noreply.github.com>
2025-11-23 12:59:01 -0600

    Create fxmanifest.lua
    defines resource and metadata deps

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Designategold7'
+description 'DPS Fire Tone Out System with Sonoran CAD & Knight Duty Integration'
+version '1.0.0'
+
+shared_script '@es_extended/imports.lua'
+
+server_script 'server.lua'
+client_script 'client.lua'
+
+dependency 'es_extended'
+dependency 'knight_duty'
