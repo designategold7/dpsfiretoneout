7bac484c38229a6c3941162797bd72db22ce9520
delineate <17305941+designategold7@users.noreply.github.com>
2026-02-06 14:53:43 -0600

    Update client.lua
    display custom msg in notif.
    + event handler to accept msgs
    +append msg to advanced notif body

--- client.lua
+++ client.lua
@@ -1,23 +1,16 @@
 local ESX = exports["es_extended"]:getSharedObject()
-
 RegisterNetEvent('dps_tone:playTone')
-AddEventHandler('dps_tone:playTone', function(coords, location)
-    -- Play Pager Tone (Native GTA Sound)
-    -- Repeat 3 times for effect
+AddEventHandler('dps_tone:playTone', function(coords, location, message)
     PlaySoundFrontend(-1, "TIMER_STOP", "HUD_MINI_GAME_SOUNDSET", 1)
     Wait(800)
     PlaySoundFrontend(-1, "TIMER_STOP", "HUD_MINI_GAME_SOUNDSET", 1)
     Wait(800)
     PlaySoundFrontend(-1, "TIMER_STOP", "HUD_MINI_GAME_SOUNDSET", 1)
-    
-    -- Set GPS Waypoint
     SetNewWaypoint(coords.x, coords.y)
-
-    -- Visual Alert
     ESX.ShowAdvancedNotification(
         'DISPATCH', 
         'FIRE TONE OUT', 
-        'Responding to: ' .. location, 
+        'Responding to: ' .. location .. message, 
         'CHAR_CALL911', 
         1
     )
